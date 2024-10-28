33fa1cb39c39879604271b100d95ce377216f2f4
Junaet Mahbub <52060704+Jdawg0309@users.noreply.github.com>
2024-10-27 22:05:33 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-module.exports = {
-    calories: 400
-}
+const express = require('express')
+const app = express()
+
+app.use(express.static("public"))
+app.use(express.urlencoded( { extended: true }))
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+const userRouter = require('./routes/data')
+
+app.use('/data', userRouter)
+
+
+app.listen(3000)
